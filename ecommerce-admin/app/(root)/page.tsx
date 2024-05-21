d45194676d5dd0567c3c0fefb163abfb732ee8af
Your Name <141324213+MorsiYoucef@users.noreply.github.com>
2024-05-21 16:30:56 +0100

    new commit

---
+++ ecommerce-admin/app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  return (
+    <div className=" flex justify-center items-center">
+      <h1>Hello admin dashboard</h1>
+      <Button>Button</Button>
+    </div>
+  )
+}
