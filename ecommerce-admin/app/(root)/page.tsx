3b4bdba4b5887cfa92b110faab2f0205134ea7dc
Your Name <141324213+MorsiYoucef@users.noreply.github.com>
2024-05-22 09:25:32 +0100

    new commit

--- ecommerce-admin/app/(root)/page.tsx
+++ ecommerce-admin/app/(root)/page.tsx
@@ -1,11 +1,14 @@
-// import Image from 'next/image'
-// import { Button } from '@/components/ui/button'
+'use client'
+
 import { UserButton } from '@clerk/nextjs'
 import '../globals.css'
+import { Modal } from '@/components/ui/modal'
 export default function SetupPage() {
   return (
     <div className=" p-4">
-      <UserButton afterSignOutUrl="/" />
+      <Modal title="Test" description="Test Desc" isOpen onClose={() => {}}>
+        Children
+      </Modal>
     </div>
   )
 }
