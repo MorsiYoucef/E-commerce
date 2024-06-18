b051ef1edb950cfd103aec5cb29887eb49c73020
Your Name <141324213+MorsiYoucef@users.noreply.github.com>
2024-06-18 15:19:43 +0100

    new commit

--- ecommerce-admin/app/(root)/page.tsx
+++ ecommerce-admin/app/(root)/page.tsx
@@ -1,15 +1,16 @@
 'use client'
 
 import { UserButton } from '@clerk/nextjs'
+// import { UserButton } from '@clerk/nextjs'
 import '../globals.css'
 import { Modal } from '@/components/ui/modal'
 import { Button } from '@/components/ui/button'
 export default function SetupPage() {
   return (
-    <div className=" p-4">
-      <Button>Click Me</Button>
-      <Modal title="Test" description="Test Desc" isOpen onClose={() => {}}>
-        Children
+    <div className="p-4 flex flex-col gap-2">
+      <UserButton afterSignOutUrl="/" />
+      <Modal title="Test" description=" Test Desc" isOpen onClose={() => {}}>
+        children
       </Modal>
     </div>
   )
