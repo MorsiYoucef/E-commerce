f85a28041da6104bd07fc01f6c80a9e78b3183e9
Your Name <141324213+MorsiYoucef@users.noreply.github.com>
2024-06-18 21:23:44 +0100

    new commit

--- ecommerce-admin/components/modals/store-modal.tsx
+++ ecommerce-admin/components/modals/store-modal.tsx
@@ -2,9 +2,27 @@
 
 import { Modal } from '@/components/ui/modal'
 import { useStoreModal } from '@/hooks/use-store-modal'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Form, FormField, FormItem, FormLabel } from '../ui/form'
+
+const formSchema = z.object({
+  name: z.string().min(1),
+})
 
 export const StoreModal = () => {
   const storeModal = useStoreModal()
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+    },
+  })
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values)
+  }
   return (
     <Modal
       title="Create Store"
@@ -12,7 +30,23 @@ export const StoreModal = () => {
       isOpen={storeModal.isOpen}
       onClose={storeModal.onClose}
     >
-      Future create store Form
+      <div>
+        <div className=" space-y-4 py-2 pb-4">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                  </FormItem>
+                )}
+              ></FormField>
+            </form>
+          </Form>
+        </div>
+      </div>
     </Modal>
   )
 }
