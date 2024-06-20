a173d2c0e8d210af710d376f7449d2d650813f4c
Your Name <141324213+MorsiYoucef@users.noreply.github.com>
2024-06-20 14:04:06 +0100

    new commit

--- ecommerce-admin/components/Navbar.tsx
+++ ecommerce-admin/components/Navbar.tsx
@@ -1,12 +1,13 @@
 import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 import { MainNav } from './main-nav'
+import { ComboboxDemo } from './ui/ComboBox'
 
 const Navbar = () => {
   return (
     <div className=" border-b">
       <div className=" flex h-16 items-center px-4">
-        <div>This will be a store switcher</div>
+        <ComboboxDemo />
         <MainNav className=" mx-6" />
         <div className=" ml-auto flex items-center space-x-4">
           <UserButton />
