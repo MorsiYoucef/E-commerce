01487e6325776f075c7e3825c04c49fc4e1f6efd
Your Name <141324213+MorsiYoucef@users.noreply.github.com>
2024-06-25 17:49:11 +0100

    new commit

--- ecommerce-admin/components/StoreSwitcher.tsx
+++ ecommerce-admin/components/StoreSwitcher.tsx
@@ -1,11 +1,16 @@
+'use client'
+
+import { Store } from '@prisma/client'
 import { ComboboxDemo } from './ui/ComboBox'
 import { PopoverTrigger } from './ui/popover'
 
 type PopoverTriggerProps = React.ComponentPropsWithoutRef<typeof PopoverTrigger>
 
-interface StoreSwitchProps extends PopoverTriggerProps {}
+interface StoreSwitchProps extends PopoverTriggerProps {
+  items: Store[]
+}
 
-const StoreSwitcher = () => {
+const StoreSwitcher = ({ className, items = [] }: StoreSwitchProps) => {
   return (
     <div>
       <ComboboxDemo />
